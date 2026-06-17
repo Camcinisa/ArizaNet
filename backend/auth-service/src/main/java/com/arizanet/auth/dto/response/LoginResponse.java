33955fcbfc3430043c76abb4b760a5b686d493c4
Nisa Camcı <nscmc34@gmail.com>
2026-06-17 11:44:11 +0300

    Complete day 4 login and role authorization

---
+++ backend/auth-service/src/main/java/com/arizanet/auth/dto/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.arizanet.auth.dto.response;
+public class LoginResponse {
+    private String token;
+    private String fullName;
+    private String role;
+
+    public LoginResponse() {
+    }
+    public LoginResponse(String token, String fullName, String role) {
+        this.token = token;
+        this.fullName = fullName;
+        this.role = role;
+    }
+    public String getToken() {
+        return token;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getRole() {
+        return role;
+    }
+}
